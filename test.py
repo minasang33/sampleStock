d1fdbed7c44e3bda5478a3f91a02bcec4f3631b8
왕혜민 <whm0413@icraft21.com>
2023-02-02 09:27:25 +0900

    test

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
- 
+
 @app.route('/')
 def main():
-    return render_template('index.html')
- 
+    str = 'whm Hello'
+    return render_template('index.html', str=str)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
